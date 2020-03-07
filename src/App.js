9639dd50c17f70719fca22a419c6dde96d0a3c44
CxLoi <loi109908@gmail.com>
2020-03-07 13:58:56 +0800

    Test github integration

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox Are you</h1>
+      <h1>Hello CodeSandbox Are you sure</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
